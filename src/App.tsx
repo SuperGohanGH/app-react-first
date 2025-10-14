9f4529ab03a27dd52f8538d32117c5e43c816af6
SuperGohanGH <adelis.rami@gmail.com>
2025-10-14 16:13:47 +0200

    firstcommit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import EsempioStile from './EsempioStile'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const nome = "Gohan"
+  const a = 10
+  const b = 20
+
+  return (
+    <>
+      <EsempioStile />
+      <h1>la suma de {a} + {b} es {a + b}</h1>
+      <h1>que pasa pe papi me llamo {nome} y tengo {count} años</h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={
+          () => setCount((count) => count + 1)
+        }>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/EsempioStile.tsx
@@ -0,0 +1,17 @@
+const EsempioStile = () => {
+    const mioStile = { 
+        backgroundColor: 'red',
+    }
+    return (
+        <>
+            <div style={mioStile}>
+                <h1 className="titolo-principale"> Habla causa</h1>
+            </div>
+            <div style={{ backgroundColor: 'red' }}>
+                <h1 className="titolo-principale"> Habla causa</h1>
+            </div>
+        </>
+    )
+}
+
+export default EsempioStile
